ae6cb62dbd3f22de178dc691d1038aa58ad57531
Todd H. Gardner <todd@wocketware.com>
2012-11-10 12:03:56 -0600

    added custom post type for projects and alternate theme for re-activiation

--- src/wp-content/themes/manoamano/functions.php
+++ src/wp-content/themes/manoamano/functions.php
@@ -220,6 +220,7 @@ add_custom_background();
 if ( function_exists('add_theme_support') )
 add_theme_support( 'menus' );
 
+
 function register_menus(){
 	register_nav_menus(
 	array(
@@ -228,6 +229,56 @@ function register_menus(){
 	)
 );}
 add_action('init','register_menus');
+
+
+// let's create the function for the custom type
+function register_project_post_type() { 
+    /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+    register_post_type( 'project', 
+        // let's now add all the options for this post type
+        array('labels' => array(
+            'name' => __('Projects', 'post type general name'), /* This is the Title of the Group */
+            'singular_name' => __('Project', 'post type singular name'), /* This is the individual type */
+            'all_items' => __('All Projects'), /* the all items menu item */
+            'add_new' => __('Add New', 'custom post type item'), /* The add new menu item */
+            'add_new_item' => __('Add New Project'), /* Add New Display Title */
+            'edit' => __( 'Edit' ), /* Edit Dialog */
+            'edit_item' => __('Edit Project'), /* Edit Display Title */
+            'new_item' => __('New Project'), /* New Display Title */
+            'view_item' => __('View Project'), /* View Display Title */
+            'search_items' => __('Search Projects'), /* Search Custom Type Title */ 
+            'not_found' =>  __('Nothing found in the Database.'), /* This displays if there are no entries yet */ 
+            'not_found_in_trash' => __('Nothing found in Trash'), /* This displays if there is nothing in the trash */
+            'parent_item_colon' => ''
+            ), /* end of arrays */
+            'description' => __( 'Geospacial project in Bolivia' ), /* Custom Type Description */
+            'public' => true,
+            'publicly_queryable' => true,
+            'exclude_from_search' => false,
+            'show_ui' => true,
+            'query_var' => true,
+            'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
+            'menu_icon' => get_stylesheet_directory_uri() . '/images/project-icon.png', /* the icon for the custom post type menu */
+            'rewrite'   => array( 'slug' => 'project' ), /*, 'with_front' => true ), /* you can specify it's url slug */
+            'has_archive' => true, /*'project',  you can rename the slug here */
+            'capability_type' => 'post',
+            'hierarchical' => false,
+            /* the next one is important, it tells what's enabled in the post editor */
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+        ) /* end of options */
+    ); /* end of register post type */
+    
+    /* this ads your post categories to your custom post type */
+    register_taxonomy_for_object_type('Airfields', 'project');
+    register_taxonomy_for_object_type('Clinics', 'project');
+    register_taxonomy_for_object_type('Roads', 'project');
+    register_taxonomy_for_object_type('Schools', 'project');
+    register_taxonomy_for_object_type('Communities', 'project');
+    
+    /* this ads your post tags to your custom post type */
+    //register_taxonomy_for_object_type('post_tag', 'custom_type');
+} 
+add_action( 'init', 'register_project_post_type');
 
 // Display home page link in custom menu
 function home_page_menu_args( $args ) {
